7a77f9bdf81d4b20344784dc4e75cceff8cb3f9e
Stephen Hellicar <s.hellicar@gmail.com>
2023-03-15 23:46:56 +1000

    Revert script.

--- install.sh
+++ install.sh
@@ -2,14 +2,17 @@
 
 # Update the list of packages
 sudo apt-get update
-
-
-# Add repositories
+# Install pre-requisite packages.
 sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
-
+# Update the list of packages after we added packages.microsoft.com
 sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
 
-
-sudo apt-get install -y powershell install nodejs
+# Install nodejs
+sudo apt update
+sudo apt install nodejs
